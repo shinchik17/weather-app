7c2895659a976a7a94beda6b11f2d2d17ffce39f
shinchik17 <shinyaev.2013@mail.ru>
2024-08-28 15:30:46 +0300

    feat: add repository for locations

---
+++ src/main/java/com/alexshin/weatherapp/repository/LocationRepository.java
@@ -0,0 +1,26 @@
+package com.alexshin.weatherapp.repository;
+
+import com.alexshin.weatherapp.entity.Location;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class LocationRepository extends BaseRepository<Long, Location> {
+
+    public LocationRepository(SessionFactory sessionFactory) {
+        super(Location.class, sessionFactory);
+    }
+
+    public List<Location> findByUserId(Long userId) {
+        return runWithinTxAndReturn(
+                session -> {
+                    String sqlString = "FROM Location l WHERE l.user.id = :userId";
+                    Query<Location> query = session.createQuery(sqlString, clazz);
+                    query.setParameter("userId", userId);
+                    return query.getResultList();
+                }
+        );
+    }
+
+}
